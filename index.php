e7ce7bed950555dd621a1247b3af09811e6b71b9
hatung01 <97158166+hatung01@users.noreply.github.com>
2024-04-19 07:25:09 +0000

    bai tap slide 73

--- index.php
+++ index.php
@@ -12,6 +12,26 @@
 		$p = 2 * PI * $r;
 		echo $p;
 		echo "<br> $s";
+		echo "<br>";
+		$n = 100;
+		$arr1 = array($n);
+		$arr2 = array(1, 2, 3, 4);
+		$arr3 = array();
+		$arr4 = array('hoten' => "HTung", 
+					'quequan' => "LX",
+					'tuoi' => 24,
+					'IQ' => "Rat cao");
+		print_r($arr4);
+		for($a = 0; $a <= 200; $a++){
+			if($a%2 == 0){
+				echo "<b><font color=red>".$a."</font></b>";
+				echo "<br>";
+			}
+			else {
+				echo "<i><font color=blue>".$a."</font></i>";
+				echo "<br>";
+			}
+		}
 		
 
 ?>

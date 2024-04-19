d686ebb1e279cac4e14dce1eeb36627ffe170301
hatung01 <97158166+hatung01@users.noreply.github.com>
2024-04-19 06:52:57 +0000

    khai bao ham

--- index.php
+++ index.php
@@ -1,27 +1,19 @@
-<?php
-
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
-
-function sayHello($name) {
-	echo "Hello $name!";
-}
-
-?>
 
 <html>
 	<head>
 		<title>Visual Studio Code Remote :: PHP</title>
 	</head>
 	<body>
-		<?php 
+	<?php
+
+		define("PI", 3.14);
+		$r=10;
+		$s= PI * pow($r ,2); 
+		$p = 2 * PI * $r;
+		echo $p;
+		echo "<br> $s";
 		
-		sayHello('remote world');
-			
-		phpinfo(); 
-			
-		?>
+
+?>
 	</body>
 </html>
